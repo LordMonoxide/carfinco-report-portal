66e8d439620d8c3a70c33f99c2e9e5478775591d
Corey Frenette <corey@narwhunderful.com>
2014-06-20 17:23:04 -0300

    Switch Admin's Dealer listing to table view

--- http/app/views/admin/dealers/view.blade.php
+++ http/app/views/admin/dealers/view.blade.php
@@ -4,19 +4,42 @@
       <div id="dealers" class="content site-block">
 				<h1>Dealers {{ HTML::linkAction('admin.dealers.new', 'Add Dealer', null, ['class' => 'btn']) }}</h1>
 				
-				<ul class="reports">
-          @foreach($dealers as $dealer)
-            <li><a href="{{ URL::action('admin.dealers.edit', $dealer->id) }}" title=""><em>{{ $dealer->name }}</em><span>{{ $dealer->user->email }}<br />{{ $dealer->number }}</span></a></li>
-          @endforeach
-				</ul>
-        
-				<form class="search">
-					<fieldset>
-						<label>Search by keyword</label>
-						<input type="text" name="" />
-						<button type="submit" class="btn">Search</button>
-					</fieldset>
-				</form>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Number</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <?php $n = 1; ?>
+            @foreach($dealers as $dealer)
+              <tr>
+                <td>{{ $n++ }}</td>
+                <td>{{ $dealer->name }}</td>
+                <td>{{ $dealer->number }}</td>
+                <td>{{ $dealer->user->email }}</td>
+                <td>{{ $dealer->phone }}</td>
+                <td>
+                  {{ Form::open(['route' => ['admin.dealers.edit', $dealer->id], 'method' => 'get']) }}
+                  {{ Form::submit('Edit') }}
+                  {{ Form::close() }}
+                </td>
+                <td>
+                  {{ Form::open(['route' => ['admin.dealers.delete', $dealer->id], 'method' => 'delete']) }}
+                  {{ Form::submit('Delete') }}
+                  {{ Form::close() }}
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
 			</div>
 		</div>
 @stop
