94c0dce8fdf36e0cbbfb0d89f964aa22159b494f
Corey Frenette <corey@narwhunderful.com>
2014-06-20 16:21:25 -0300

    Change admin view to table

--- http/app/views/root/admins/view.blade.php
+++ http/app/views/root/admins/view.blade.php
@@ -4,11 +4,36 @@
       <div id="admins" class="content site-block">
 				<h1>Admins {{ HTML::linkAction('root.admins.new', 'Add Admin', null, ['class' => 'btn']) }}</h1>
 				
-				<ul class="admins">
-          @foreach($admins as $admin)
-            <li><a href="{{ URL::action('root.admins.edit', $admin->id) }}" title=""><span>{{ $admin->user->email }}</span></a></li>
-          @endforeach
-				</ul>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Email</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <?php $n = 1; ?>
+            @foreach($admins as $admin)
+              <tr>
+                <td>{{ $n++ }}</td>
+                <td>{{ $admin->user->email }}</td>
+                <td>
+                  {{ Form::open(['route' => ['root.admins.edit', $admin->id], 'method' => 'get']) }}
+                  {{ Form::submit('Edit') }}
+                  {{ Form::close() }}
+                </td>
+                <td>
+                  {{ Form::open(['route' => ['root.admins.delete', $admin->id], 'method' => 'delete']) }}
+                  {{ Form::submit('Delete') }}
+                  {{ Form::close() }}
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
         
 				<form class="search">
 					<fieldset>
